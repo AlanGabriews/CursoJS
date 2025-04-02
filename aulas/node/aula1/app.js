2b36f9aaf48328c7dd6b59fff52e551acbd0c00f
AlanGabriews <ms.alangabriel@gmail.com>
2025-04-02 12:12:51 -0300

    aula 65 filter

---
+++ aulas/node/aula1/app.js
@@ -0,0 +1,5 @@
+const { Pessoa, num2 } = require("./mod1");
+
+const p1 = new Pessoa("alan");
+
+console.log(p1.nome);
---
+++ aulas/node/aula1/mod1.js
@@ -0,0 +1,11 @@
+class Pessoa
+{
+    constructor(nome)
+    {
+        this.nome = nome;
+    }
+}
+
+exports.Pessoa = Pessoa;
+exports.num1 = 1;
+exports.num2 = 2;
